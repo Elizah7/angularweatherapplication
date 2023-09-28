d733a5ced4fae507ad84d981e6c9eb0f1f8dd59e
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2023-09-28 12:38:55 +0530

    ccreatedtheangularapp

---
+++ weather-app/src/app/MyComponents/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  sliderImages: string[] = [
+    'https://assets-in.bmscdn.com/promotions/cms/creatives/1689679473819_horrordesktop.jpg',
+    'https://assets-in.bmscdn.com/promotions/cms/creatives/1688970894022_bigweb.jpg',
+    'https://assets-in.bmscdn.com/promotions/cms/creatives/1688363625984_delhitheatredesktop.jpg',
+    'https://assets-in.bmscdn.com/promotions/cms/creatives/1689060789408_fatimaayeshadesktop.jpg',
+  ];
+
+
+
+
+}
+
+
+let init = {
+  product :[]
+}
+
+// const reducer = (state,action)=>{
+//      switch case (action.payload) :{
+          
+//      }
+// }
---
+++ weather-app/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './MyComponents/navbar/navbar.component';
+
+import {HomeComponent} from './MyComponents/home/home.component'
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog'; 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DetailspageComponent } from './MyComponents/detailspage/detailspage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DetailspageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    CarouselModule.forRoot(),
+    HttpClientModule,
+    MatDialogModule,
+    NgbModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
